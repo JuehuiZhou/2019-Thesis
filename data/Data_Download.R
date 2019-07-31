2c4350ba60ad948d58b4ddaf67a8a043005ecc3c
Juehui Zhou <juehui.zhou@hdr.mq.edu.au>
2019-07-31 10:36:51 +1000

    Data imported and ignored

---
+++ data/Data_Download.R
@@ -0,0 +1,10 @@
+library(readxl)
+SecondBite <- read_excel("~/Documents/2019 Thesis Writing/Data/SecondBite.xlsx")
+View(SecondBite) 
+
+OzHarvest <- read_excel("~/Documents/2019 Thesis Writing/Data/OzHarvest.xlsx")
+View(OzHarvest)
+
+FoodBank_NSW_and_ACT_limited <- read_excel("~/Documents/2019 Thesis Writing/Data/FoodBank NSW and ACT limited.xlsx")
+View(FoodBank_NSW_and_ACT_limited)
+
---
+++ scripts/Git_Setup.R
@@ -0,0 +1,2 @@
+system('git config --global user.email "juehui.zhou@hdr.mq.edu.au"')
+system('git config --global user.name "Juehui Zhou"')
