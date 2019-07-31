7a628bb50a599ff072971ac899459b8794ca33ea
Juehui Zhou <juehui.zhou@hdr.mq.edu.au>
2019-07-31 11:52:12 +1000

    plot ozharvest revenue

--- data/Data_Download.R
+++ data/Data_Download.R
@@ -8,3 +8,13 @@ View(OzHarvest)
 FoodBank_NSW_and_ACT_limited <- read_excel("~/Documents/2019 Thesis Writing/Data/FoodBank NSW and ACT limited.xlsx")
 View(FoodBank_NSW_and_ACT_limited)
 
+install.packages("tidyverse")
+library(ggplot2)
+
+ozharvest <- read_excel("~/Documents/2019 Thesis Writing/Data/OzHarvest.xlsx") 
+ozharvest
+
+ggplot(data = ozharvest, aes(x = Year, y= Revenue)) +
+  geom_line()
+
+  
